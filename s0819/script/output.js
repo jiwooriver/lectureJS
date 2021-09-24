ad0e6ebbd314f86a4c87b4102af10fb5f607511b
jiwooriver <jiwooriver@gmail.com>
2021-09-24 23:52:51 +0900

    두번째 커밋

---
+++ s0819/script/output.js
@@ -0,0 +1,9 @@
+document.getElementById("out").innerHTML
+= "<pre>// html요소로 출력 작성\ndocument.getElementById(\"demo\").innerHTML</pre>";
+
+document.write("<pre>// document 출력으로 작성 \ndocument.write()</pre>");
+
+window.alert("// window객체 경고창을 사용하여 출력\nwindow.alert()");
+
+let logWrite = "// 브라우저 콘솔을 사용하여 출력 =>> console.log()"
+console.log(logWrite);
